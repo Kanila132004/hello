bc77d60b63f5dc424644d3effa6e699fb57db9e5
Kanila132004 <vijayakumarkanila@gmail.com>
2023-03-09 22:22:23 +0530

    positive or negative

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-print("Hello kanila")
-print("hi hello")
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
